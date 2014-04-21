04458e097654d378074d4adce14df6c5b225baa1
jagob <jacobvdueholm@hotmail.com>
2014-04-21 15:47:01 +0200

    mutt mailboxes

---
+++ startup.m
@@ -0,0 +1,14 @@
+% userpath
+% userpath('/home/jacob/.matlab/R2013a/')
+
+% cvx
+% first time run cvx_setup, check userpath
+run /home/jacob/.matlab/R2013a/cvx/cvx_startup.m
+
+format long
+format compact
+
+addpath /home/jacob/documents/svn/p6/code/matlab_generic
+addpath /home/jacob/documents/svn/p6/code/matlab_gfsk_workbench
+addpath /home/jacob/documents/svn/p6/code/matlab_gfsk_workbench/modulator
+addpath /home/jacob/documents/svn/p6/code/matlab_gfsk_workbench/demodulator
