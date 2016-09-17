f26f52321f657c3d4635fc9f127a46d358157f82
jagob <jacobvdueholm@gmail.com>
2016-09-17 23:03:36 +0200

    install

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ mkdir downloads documents music .Dropboxdata
 ln -s ~/dotfiles/.xinitrc .
 ln -s ~/dotfiles/.xmonad .
 ln -s ~/dotfiles/.stalonetrayrc .
-ln -s ~/dotfiles/.Xresoruces .
+ln -s ~/dotfiles/.Xresources .
 ln -s ~/dotfiles/.zshrc .
 ln -s ~/dotfiles/.vimrc .
 ln -s ~/dotfiles/.vim .
@@ -18,11 +18,30 @@ ln -s ~/dotfiles/.ncmpcpp .
 ln -s ~/dotfiles/bin .
 ln -s ~/dotfiles/zathurarc .
 
+
+sudo pacman -S zsh htop firefox flashplugin unzip thunar-archive-plugin file-roller gvfs thunar-volman tumbler gvim ntp texlive-most vim-latexsuite zathura zathura-pdf-poppler evince kdegraphics-okular gst-libav vim-spell-da subversion vlc lib32-libpulse jre7-openjdk jdk7-openjdk icedtea-web transmission-gtk inkscape gimp ntfs-3g wine wine-mono wine_gecko lib32-ncurses feh eog cups cups-pdf system-config-printer samba xorg-xrandr arandr ncmpcpp mpd mpc libreoffice-still cmake opencv ffmpeg ipython2 python2-pygame python2-numpy python2-scipy python2-matplotlib python2-pyqt4 python2-h5py flake8 wxpython python2-pyzmq python2-jinja python2-tornado youtube-dl cheese tk ttf-ubuntu-font-family irssi wmctrl lxappearance lynx maven bc gmrun offlineimap thunderbird xfce4 gvfsin xfce4-notifyd inotify-tools autojump dmenu mutt
+
+git clone https://aur.archlinux.org/package-query.git && cd package-query && makepkg && sudo pacman -U *.pkg.tar.xz
+
+git clone https://aur.archlinux.org/yaourt.git && cd yaourt && makepkg && sudo pacman -U *.tar.xz
+
+yaourt -S --noconfirm xcursor-human ttf-ms-fonts dropbox dropbox-cli gnome-keyring-query urlview pdfjumbler archey urxvt-font-size-git openprinting-ppds-pxlcolor-generic google-chrome
+
+chsh -s $(which zsh)
+
+
 touch ~/.mpd/{database,log,pid,state,sticker.sql,errorlog}
+
+
+
 
 mkdir -p ~/.mail ~/.mail/jacobgmail ~/.mail/aau
 echo 'mailboxes "+jacobgmail/INBOX" "+jacobgmail/[Gmail].Sent Mail" "+jacobgmail/gemte" "+jacobgmail/Drafts" "+jacobgmail/Trash"' > ~/.mail/jacobgmail/mailboxes
 echo 'mailboxes "+aau/INBOX" "+aau/Sent Items" "+aau/Drafts"' > ~/.mail/aau/mailboxes
+
+
+
+
 
 sudo cp /etc/samba/smb.conf.default /etc/samba/smb.conf
 
