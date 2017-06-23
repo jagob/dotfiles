d8446029c4f0c70ca7c23f237cb71bbbed60e0fa
jagob <jacobvdueholm@gmail.com>
2017-06-23 01:28:50 +0000

    updated install

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -160,11 +160,18 @@ myDzenStyle = "-h 20 -fg '"++myDzenFGColor++"' -bg '"++myDzenBGColor++"' -fn '"+
 -- myBotBar    = "conky -c ~/.xmonad/dzen2/.conky_dzen_bot   | dzen2 -x 0 -y 1030 -w 1680 -ta l "      ++ myDzenStyle
 -- myTrayer    = "trayer --edge top --align left --margin 1250 --widthtype pixel --width 100 --SetDockType true --SetPartialStrut false --expand false --heighttype pixel --height 20 --transparent true --tint 0xfdf6e3 --alpha 0"
 
--- 1920x1080
+-- -- 1920x1080
+-- myStartBar  = "conky -c ~/.xmonad/dzen2/.conky_start_apps | dzen2 -x 0 -y 0 -w 30 -ta l"            ++ myDzenStyle
+-- myStatusBar =                                              "dzen2 -e '' -x 30 -y 0 -w 1220 -ta l "  ++ myDzenStyle
+-- myTopBar    = "conky -c ~/.xmonad/dzen2/.conky_dzen_top   | dzen2 -e '' -x 1350 -y 0 -w 570 -ta r " ++ myDzenStyle
+-- myBotBar    = "conky -c ~/.xmonad/dzen2/.conky_dzen_bot   | dzen2 -x 0 -y 1060 -w 1920 -ta l "      ++ myDzenStyle
+-- myTrayer    = "trayer --edge top --align left --margin 1250 --widthtype pixel --width 100 --SetDockType true --SetPartialStrut false --expand false --heighttype pixel --height 20 --transparent true --tint 0xfdf6e3 --alpha 0"
+
+-- 2560x1440
 myStartBar  = "conky -c ~/.xmonad/dzen2/.conky_start_apps | dzen2 -x 0 -y 0 -w 30 -ta l"            ++ myDzenStyle
 myStatusBar =                                              "dzen2 -e '' -x 30 -y 0 -w 1220 -ta l "  ++ myDzenStyle
-myTopBar    = "conky -c ~/.xmonad/dzen2/.conky_dzen_top   | dzen2 -e '' -x 1350 -y 0 -w 570 -ta r " ++ myDzenStyle
-myBotBar    = "conky -c ~/.xmonad/dzen2/.conky_dzen_bot   | dzen2 -x 0 -y 1060 -w 1920 -ta l "      ++ myDzenStyle
+myTopBar    = "conky -c ~/.xmonad/dzen2/.conky_dzen_top   | dzen2 -e '' -x 1350 -y 0 -w 1210 -ta r " ++ myDzenStyle
+myBotBar    = "conky -c ~/.xmonad/dzen2/.conky_dzen_bot   | dzen2 -x 0 -y 1420 -w 2560 -ta l "      ++ myDzenStyle
 myTrayer    = "trayer --edge top --align left --margin 1250 --widthtype pixel --width 100 --SetDockType true --SetPartialStrut false --expand false --heighttype pixel --height 20 --transparent true --tint 0xfdf6e3 --alpha 0"
 
 -- Define new key combinations to be added
--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 cd
 
-mkdir downloads documents music work .Dropboxdata 
+mkdir downloads documents music work .Dropboxdata .local/share/applications
 
 ln -s ~/dotfiles/.xinitrc .
 ln -s ~/dotfiles/.xmonad .
@@ -19,21 +19,19 @@ ln -s ~/dotfiles/bin .
 ln -s ~/dotfiles/zathurarc .
 ln -s ~/dotfiles/vim-urxt.desktop ~/.local/share/applications/
 
+sudo pacman -S zsh htop firefox flashplugin unzip thunar-archive-plugin file-roller gvfs thunar-volman tumbler ntp texlive-most vim-latexsuite zathura zathura-pdf-poppler evince okular gst-libav vim-spell-da vlc lib32-libpulse jre7-openjdk jdk7-openjdk icedtea-web inkscape gimp ntfs-3g wine wine-mono wine_gecko lib32-ncurses feh eog cups cups-pdf system-config-printer samba xorg-xrandr arandr ncmpcpp mpd mpc libreoffice-still cmake opencv ffmpeg ipython2 python2-ipdb python2-pygame python2-numpy python2-scipy python2-matplotlib python2-pyqt4 python2-h5py flake8 wxpython python2-pyzmq python2-jinja python2-tornado youtube-dl cheese tk irssi wmctrl lxappearance lynx maven bc gmrun offlineimap xfce4 xfce4-notifyd inotify-tools autojump dmenu mutt mpg123
+# outdated: gvfsin 
+cd ~/downloads && git clone https://aur.archlinux.org/package-query.git && cd package-query && makepkg && sudo pacman -U *.pkg.tar.xz
 
-sudo pacman -S zsh htop firefox flashplugin unzip thunar-archive-plugin file-roller gvfs thunar-volman tumbler ntp texlive-most vim-latexsuite zathura zathura-pdf-poppler evince kdegraphics-okular gst-libav vim-spell-da vlc lib32-libpulse jre7-openjdk jdk7-openjdk icedtea-web inkscape gimp ntfs-3g wine wine-mono wine_gecko lib32-ncurses feh eog cups cups-pdf system-config-printer samba xorg-xrandr arandr ncmpcpp mpd mpc libreoffice-still cmake opencv ffmpeg ipython2 python2-ipdb python2-pygame python2-numpy python2-scipy python2-matplotlib python2-pyqt4 python2-h5py flake8 wxpython python2-pyzmq python2-jinja python2-tornado youtube-dl cheese tk irssi wmctrl lxappearance lynx maven bc gmrun offlineimap xfce4 gvfsin xfce4-notifyd inotify-tools autojump dmenu mutt mpg123
+cd ~/downloads && git clone https://aur.archlinux.org/yaourt.git && cd yaourt && makepkg && sudo pacman -U *.pkg.tar.xz
 
-git clone https://aur.archlinux.org/package-query.git && cd package-query && makepkg && sudo pacman -U *.pkg.tar.xz
+yaourt -S --noconfirm xcursor-human ttf-ms-fonts ttf-ubuntu-mono-derivative-powerline dropbox dropbox-cli gnome-keyring-query urlview pdfjumbler urxvt-font-size-git openprinting-ppds-pxlcolor-generic google-chrome 
+# skype gcc-multilib fremfor regulær gcc
 
-git clone https://aur.archlinux.org/yaourt.git && cd yaourt && makepkg && sudo pacman -U *.tar.xz
-
-yaourt -S --noconfirm xcursor-human ttf-ms-fonts dropbox dropbox-cli gnome-keyring-query urlview pdfjumbler urxvt-font-size-git openprinting-ppds-pxlcolor-generic google-chrome skype
 
 chsh -s $(which zsh)
 
 touch ~/.mpd/{database,log,pid,state,sticker.sql,errorlog}
-
-
-
 
 mkdir -p ~/.mail ~/.mail/jacobgmail ~/.mail/aau
 echo 'mailboxes "+jacobgmail/INBOX" "+jacobgmail/[Gmail].Sent Mail" "+jacobgmail/gemte" "+jacobgmail/Drafts" "+jacobgmail/Trash"' > ~/.mail/jacobgmail/mailboxes
